146937305e101a108beccbe340cdcd0ce339e7b2
Billy <ryotabilly@gmail.com>
2024-02-03 18:00:04 -0800

    feat(docker-compose):add dockerfiles and docker-compose and enable hot-reload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  preview: {
+   port: 3000,
+   strictPort: true,
+  },
+  server: {
+   port: 3000,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:3000",
+  },
+  
 })
